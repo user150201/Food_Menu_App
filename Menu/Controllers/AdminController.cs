242156a69805d81f7d3499d35bd76ef1f063a2ef
Gokul <gokulr2087@gmail.com>
2025-02-27 23:45:50 +0530

    Add new Dish feature updated

---
+++ Menu/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Menu.Data;
+using Menu.Models;
+
+namespace Menu.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly MenuContext _context;
+
+        public AdminController(MenuContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Dish dish)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Dishes.Add(dish);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Home"); // Redirect to Home after adding
+            }
+            return View(dish);
+        }
+    }
+}
